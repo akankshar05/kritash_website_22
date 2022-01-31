5eccefa4445c1cc68e7a3c22d5d51e8b47d71db9
prashantgautam96 <2020ucs0114@iitjammu.ac.in>
2022-02-01 00:46:46 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import "tailwindcss/dist/base.css";
+import "styles/globalStyles.css";
+import React from "react";
+import { css } from "styled-components/macro"; //eslint-disable-line;
+
+import EventLandingPage from "demos/EventLandingPage.js";
+import ComponentRenderer from "ComponentRenderer.js";
+import MainLandingPage from "MainLandingPage.js";
+import ThankYouPage from "ThankYouPage.js";
+import BlogIndexPage from "pages/BlogIndex.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Aksharsala from "pages/Aksharsala";
+import Team from "pages/Team";
+import AboutUs from "pages/AboutUs";
+import Events from "pages/Events";
+export default function App() {
+  // If you want to disable the animation just use the disabled `prop` like below on your page's component
+  // return <AnimationRevealPage disabled>xxxxxxxxxx</AnimationRevealPage>;
+
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/components/:type/:subtype/:name">
+          <ComponentRenderer />
+        </Route>
+        <Route path="/components/:type/:name">
+          <ComponentRenderer />
+        </Route>
+        <Route path="/blogging">
+          <BlogIndexPage />
+        </Route>
+        <Route path="/accha">
+          <Aksharsala/>
+        </Route>
+        <Route path="/team">
+          <Team/>
+        </Route>
+        <Route path="/team">
+        <AboutUs/>
+        </Route>
+        <Route path="/">
+          <EventLandingPage />  
+         
+        </Route>
+        <Route path="/aksharsala">
+          <Aksharsala />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
---
+++ src/pages/Team.js
@@ -0,0 +1,33 @@
+import React from "react";
+import tw from "twin.macro";
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+import Hero from "components/hero/BackgroundAsImage.js";
+import Footer from "components/footers/SimpleFiveColumn.js";
+import Ourteam from "components/cards/ProfileThreeColGrid";
+import ContactUs from "./ContactUs";
+
+const HighlightedText = tw.span`text-primary-500`
+
+export default () => (
+  <AnimationRevealPage>
+    <Hero />
+    
+    <HighlightedText>
+        2020
+    </HighlightedText>
+    <Ourteam/>
+    <HighlightedText>
+        2019
+    </HighlightedText>
+
+    <Ourteam/>
+   <HighlightedText>
+        2018
+    </HighlightedText>
+    <Ourteam/>
+    
+    
+    <Footer />
+    
+  </AnimationRevealPage>
+);
